256dd3ab73f58e55158e82841adb163260c049ff
ubuntu <ubuntu@localhost.localdomain>
2018-05-18 01:07:49 +0800

    init nginx

---
+++ nginx/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if docker ps -a | grep nginx
+then
+	docker rm -f nginx
+fi
+
+docker run -it -d \
+	--name nginx \
+	-p 80:80 \
+	-v ~/docker/nginx/config:/etc/nginx/conf.d \
+	-v ~/docker/nginx/log:/var/log/nginx \
+	nginx
